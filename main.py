94b28f6bad4ce4f944eff982e8994de6a1e15ce3
EmilioM <lemiliom@gmail.com>
2024-06-06 16:03:50 -0600

    new commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from unittest import main
 
 print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 # print(mean_var_std.calculate([2,6,2,8,4,0,1,5]))
+#
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
